63fff9db6878db88c7c2c2a1dccbc34effec9b93
ArtemSoldatovV <solvladislav777@gmail.com>
2024-11-30 18:20:10 +0300

    Add files via upload

--- Multi_threading.java
+++ Multi_threading.java
@@ -23,31 +23,14 @@ public class Multi_threading {
         }
     }
 
-    class Animal_cycle implements Runnable{//животные
-        @Override
-        public void run() {
-            Map map_e = map;
-            map_e.Animals_in_the_beginning();
-            while(true){
-                try{
-                    while (true) {
-                        for (int i=0;i<10;i++){
-                            map_e.bypassing_sthe_map();
-                        }
-                        map_e = exchanger_map.exchange(map);
-                    }
-                }
-                catch (InterruptedException e) {}
-            }
-        }
-    }
 
     class withdrawal_of_islands implements Runnable{//вывод
         @Override
         public void run() {
+            Map map_e = map;
             while(true){
-                
-                    Map map_e = map;
+                    
+                    map_e.bypassing_sthe_map();
                     map_e.draw_a_map(0,10,0,10);
                 
             }
@@ -56,7 +39,6 @@ public class Multi_threading {
 
     void start() {
         new Thread(new Plant_growth()).start();
-        new Thread(new Animal_cycle()).start();
         new Thread(new withdrawal_of_islands()).start();
       }
 }
